9d1789c141ce02178f30100879ee967b3422d60a
Roi Doron <roidoron6@gmail.com>
2024-11-24 12:05:48 +0200

    Implant scss and add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+import '../src/assets/style/main.scss'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { Lobby } from './pages/Lobby'
 import { Codeblock } from './pages/Codeblock'
+import { AppHeader } from './cmps/AppHedear'
+import { AppFooter } from './cmps/AppFooter'
+
 
 export function App() {
   return (
     <>
       <Router>
+        <AppHeader/>
         <main>
           <Routes>
             <Route path="/" element={<Lobby/>} />
             <Route path="/codeblock" element={<Codeblock/>} />
           </Routes>
         </main>
+        <AppFooter/>
       </Router>
     </>
   )
