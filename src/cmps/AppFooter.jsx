8cd4a959a56dfd33e4020220df3d3e54342129af
Roi Doron <roidoron6@gmail.com>
2024-11-24 12:04:34 +0200

    Add footer and header

---
+++ src/cmps/AppFooter.jsx
@@ -0,0 +1,9 @@
+
+
+export function AppFooter() {
+    return (
+        <footer className="app-footer">
+            <h5>© 2024, Roi Doron</h5>
+        </footer>
+    )
+}
---
+++ src/cmps/AppHedear.jsx
@@ -0,0 +1,9 @@
+
+
+export function AppHeader() {
+    return (
+        <header className="app-header">
+            <h1>Coding With Tom</h1>
+        </header>
+    )
+}
