ff1851ef4a4ab889e3d8850b37603e49657ff54d
Roi Doron <roidoron6@gmail.com>
2024-11-25 10:45:20 +0200

    Refactor code block with text editor

--- src/pages/Codeblock.jsx
+++ src/pages/Codeblock.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react"
+import { codeServiceLocal } from "../services/code-block.service-local"
+import { useNavigate, useParams } from "react-router"
+import { TextEditor } from "../cmps/TextEditor"
 
 
-export function Codeblock(){
-    return(
-        <h1>hi im Codeblock</h1>
+export function Codeblock() {
+    const navigate = useNavigate()
+    const [code, setCode] = useState()
+    const { codeId } = useParams()
+    const mentor = false // need to continue
+
+    useEffect(() => {
+        if (!code) loadCode()
+    }, [])
+
+    function loadCode() {
+        console.log('hi')
+        codeServiceLocal.getById(codeId)
+            .then(code => {
+                setCode(code)
+            })
+            .catch(err => {
+                console.log('Hade issues wite loading code', err)
+                navigate('/')
+            })
+    }
+
+    if (!code) return <div>Loading ...</div>
+    return (
+        <section className="code-block  flex align-center column">
+            <h1>{code.name}</h1>
+            <TextEditor codeToEdit={code} isMentor={mentor}/>
+        </section>
     )
 }
