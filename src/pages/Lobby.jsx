869a5019b9eb1f2053efcc62102c48fc704c2f43
Roi Doron <roidoron6@gmail.com>
2024-11-25 10:43:02 +0200

    Lobby logic

--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
+import { codeServiceLocal } from "../services/code-block.service-local";
 
 
 export function Lobby() {
+    const [codes, setCodes] = useState()
+
+    useEffect(() => {
+        if (!codes) loadCodes()
+    }, [])
+
+    function loadCodes() {
+        console.log('load code')
+        codeServiceLocal.query().then(codes => setCodes(codes))
+    }
     return (
         <section>
             <h1>Choose code block</h1>
-            <Link to={'/codeblock'}>
-            <h3>Async case</h3>
-            </Link>
+
+            {codes ?
+                (codes.map(code => (
+                    <Link to={`/codeblock/${code._id}`}>
+                        <h3>{code.name}</h3>
+                    </Link>)))
+                : <div>is loading</div>
+            }
+
         </section>
     )
 }
