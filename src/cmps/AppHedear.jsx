1de11cf55e639249e44ba2d95dc0f0f07563a235
Roi Doron <roidoron6@gmail.com>
2024-11-25 10:42:25 +0200

    Add navigation to app header

--- src/cmps/AppHedear.jsx
+++ src/cmps/AppHedear.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router";
 
 
 export function AppHeader() {
     return (
         <header className="app-header">
+            <Link to='/'>
             <h1>Coding With Tom</h1>
+            </Link>
         </header>
     )
 }
