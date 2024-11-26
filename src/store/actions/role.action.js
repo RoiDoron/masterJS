c8ac95d93fb73eee9621947caff9a4c099dfaeec
Roi Doron <roidoron6@gmail.com>
2024-11-27 00:17:48 +0200

    Add student count to store

--- src/store/actions/role.action.js
+++ src/store/actions/role.action.js
@@ -1,7 +1,10 @@
-import { SET_ROLE } from "../reducers/role.reducer";
+import { SET_ROLE, SET_STUDENT_COUNT } from "../reducers/role.reducer";
 import { store } from "../store";
 
 
 export function setRole(role) {
     store.dispatch({ type: SET_ROLE, role })
+}
+export function setStudentCount(count) {
+    store.dispatch({ type: SET_STUDENT_COUNT, count })
 }
--- src/store/reducers/role.reducer.js
+++ src/store/reducers/role.reducer.js
@@ -1,14 +1,18 @@
 
 export const SET_ROLE = 'SET_ROLE'
+export const SET_STUDENT_COUNT = 'SET_STUDENT_COUNT'
 
 const initialState = {
-    role: ''
+    role: '',
+    studentCount: 0
 }
 
 export function roleReducer(state = initialState, action = {}) {
     switch (action.type) {
         case SET_ROLE:
             return { ...state, role: action.role }
+        case SET_STUDENT_COUNT:
+            return { ...state, studentCount: action.studentCount }
         default:
             return state;
     }
