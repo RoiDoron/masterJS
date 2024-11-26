664abb334bb0cbb9a085270d6487877d8e664beb
Roi Doron <roidoron6@gmail.com>
2024-11-26 21:54:13 +0200

    Add socket to text editor

--- src/cmps/TextEditor.jsx
+++ src/cmps/TextEditor.jsx
@@ -1,39 +1,59 @@
 import { useState } from "react"
-import { UnControlled as CodeMirror } from 'react-codemirror2'
+import { Controlled as CodeMirror } from 'react-codemirror2'
 import 'codemirror/lib/codemirror.css'; // Import the base CodeMirror styles
 import 'codemirror/mode/javascript/javascript'; // Import the JavaScript mode
 import { UserMsg } from "./UserMsg";
 import { showSuccessMsg } from "../services/event-bus.service";
+import { useDispatch } from "react-redux";
+import { getActionEditCode } from "../store/actions/code.action";
+import { SOCKET_EVENT_EDIT_CODE } from "../services/socket.service";
 
-export function TextEditor({ initialCode, role, solution }) {
-    const [code, setCode] = useState(initialCode)
+export function TextEditor({ initialCode, role, solution, codeEdit }) {
+    const [code1, setCode] = useState(initialCode)
 
-    function handelChange(newCode){
+    const dispatch = useDispatch()
+
+    useState(() => {
+        socketService.on(SOCKET_EVENT_EDIT_CODE, (code) => {
+            dispatch(getActionEditCode(code))
+            console.log(code);
+
+            setCode(code.studentCode)
+        })
+
+        return () => {
+            socketService.off(SOCKET_EVENT_EDIT_CODE)
+        }
+        
+    }, [code1])
+
+    function handleChange(newCode) {
         setCode(newCode)
+        codeEdit(newCode)
         if (newCode === solution) success()
     }
 
     function success() {
         showSuccessMsg('Congratulation! you are correct!')
     }
-    function isMentor(){
-        if(role === 'instructor') return true
+
+    function isMentor() {
+        if (role === 'instructor') return true
         else return false
     }
 
     return (
         <>
             <CodeMirror
-                value={code}
+                value={code1}
                 options={{
                     mode: 'javascript',
                     theme: 'material',
                     lineNumbers: true,
-                    readOnly: isMentor() 
+                    readOnly: isMentor(),
                 }}
-                onChange={(editor, data,value) => {
-                    handelChange(value)
-
+                onBeforeChange={(editor, data, value) => {
+                    handleChange(value);
                 }}
             />
             <UserMsg />
